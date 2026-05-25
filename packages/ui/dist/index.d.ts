f9a8bc871ad9802d3885a8d5a34c0653c6b1556c
Aqil Wafi <m.aqil.wafi@gmail.com>
2026-05-25 20:31:53 +0700

    update packages

--- packages/ui/dist/index.d.ts
+++ packages/ui/dist/index.d.ts
@@ -1,3 +1,12 @@
 export * from './stats';
 export * from "./button";
+export * from "./input";
+export * from "./textarea";
+export * from "./select";
+export * from "./radio";
+export * from "./checkbox";
+export * from "./label";
+export * from "./card";
+export * from "./modal";
+export * from "./tabel";
 //# sourceMappingURL=index.d.ts.map
