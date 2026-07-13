170dbf8132eec7c1db35274443e4db92f2e5ad3d
Aqil Wafi <m.aqil.wafi@gmail.com>
2026-07-13 08:39:54 +0700

    chore: remove packages/tes from track and update gitignore

--- packages/tree-simple.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Recurse -Force |
-    Where-Object { $_.FullName -notmatch '\\(node_modules|\.next|\.git)\\' } |
-    ForEach-Object { $_.FullName.Substring((Get-Location).Path.Length) }
