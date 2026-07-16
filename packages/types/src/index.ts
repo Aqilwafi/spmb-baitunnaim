fbc25f109974646c39f27cca9431802cd0fa0fc5
Aqil Wafi <m.aqil.wafi@gmail.com>
2026-07-16 13:16:20 +0700

    update types: fix error build: /spmb/* delete files

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,5 +3,3 @@ export * from './publikasi/post.type'
 export * from './shared/supabase';
 export * from './shared/core.types';
 export * from './shared/master.types';
-export * from './spmb/pendaftaran-core.types'
-export * from './spmb/siswa-core.types'
