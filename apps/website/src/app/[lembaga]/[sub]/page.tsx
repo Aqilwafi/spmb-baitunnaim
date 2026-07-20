fd0281c50ebcf850cb9c938876fb84de22b4e7d0
Aqil Wafi <m.aqil.wafi@gmail.com>
2026-07-20 16:05:38 +0700

    update: web utama: error lembaga page

--- apps/website/src/app/[lembaga]/[sub]/page.tsx
+++ apps/website/src/app/[lembaga]/[sub]/page.tsx
@@ -2,21 +2,22 @@ import Navbar from "@/components/Navbar";
 import Hero from '@/components/Hero';
 import Footer from "@/components/Footer";
 import PostDetail from "@/components/PostDetail";
+import { fetchPublishedPostBySlug } from "@/features/posts";
 
 interface Props {
-  params: Promise<{ slug: string }>; // ← params adalah Promise
+  params: Promise<{ slug: string }>;
 }
 
-// Server Component dengan async
 export default async function SubLembagaPage({ params }: Props) {
-  const { slug } = await params; // ← await params dulu
-  
+  const { slug } = await params;
+  const post = await fetchPublishedPostBySlug(slug);
+
   return (
     <div className="flex flex-col min-h-screen text-black bg-white">
       <Navbar />
       <main className="flex-1 pt-22">
         <Hero />
-        <PostDetail slug={slug} />
+        <PostDetail post={post} />
       </main>
       <Footer />
     </div>
