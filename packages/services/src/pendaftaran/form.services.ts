3c92210cd5c034942f4a267dbbc2982a95670eed
Aqil Wafi <m.aqil.wafi@gmail.com>
2026-08-19 17:10:50 +0700

    updates

--- packages/services/src/pendaftaran/form.services.ts
+++ packages/services/src/pendaftaran/form.services.ts
@@ -5,6 +5,7 @@ import "server-only";
 import { createSupabaseServer } from "@bn/supabase";
 import { FormPendaftaran } from "@bn/types";
 
+// ambil semua form untuk admin
 export async function getFormPendaftaran(): Promise<FormPendaftaran[]> {
 
   const supabase = await createSupabaseServer();
@@ -12,6 +13,37 @@ export async function getFormPendaftaran(): Promise<FormPendaftaran[]> {
     .from('form_pendaftaran')
     .select('*')
     .is('deleted_at', null);
+
+  if (error) return [];
+
+  return data;
+}
+
+// ambil semua form untuk pendaftar berdasarkan tahun ajaran
+export async function getFormPendaftaranBySiswaIdsAndTahunAjaranId(siswaIds: string[], tahunAjaranId: number): Promise<FormPendaftaran[]> {
+
+  const supabase = await createSupabaseServer();
+  const { data, error } = await supabase
+    .from('form_pendaftaran')
+    .select('*')
+    .is('deleted_at', null)
+    .in('biodata_siswa_id', siswaIds)
+    .eq('tahun_ajaran_id', tahunAjaranId);
+
+  if (error) return [];
+
+  return data;
+}
+
+export async function getFormPendaftaranByPendaftarIdAndTahunAjaranId(pendaftarId: string, tahunAjaranId: number): Promise<FormPendaftaran[]> {
+
+  const supabase = await createSupabaseServer();
+  const { data, error } = await supabase
+    .from('form_pendaftaran')
+    .select('*')
+    .is('deleted_at', null)
+    .eq('pendaftar_id', pendaftarId)
+    .eq('tahun_ajaran_id', tahunAjaranId);
 
   if (error) return [];
 
@@ -40,36 +72,6 @@ export async function getFormPendaftaranByPendaftarId(pendaftarId: string): Prom
     .select('*')
     .is('deleted_at', null)
     .eq('pendaftar_id', pendaftarId);
-
-  if (error) return [];
-
-  return data;
-}
-
-export async function getFormPendaftaranByPendaftarIdAndTahunAjaranId(pendaftarId: string, tahunAjaranId: number): Promise<FormPendaftaran[]> {
-
-  const supabase = await createSupabaseServer();
-  const { data, error } = await supabase
-    .from('form_pendaftaran')
-    .select('*')
-    .is('deleted_at', null)
-    .eq('pendaftar_id', pendaftarId)
-    .eq('tahun_ajaran_id', tahunAjaranId);
-
-  if (error) return [];
-
-  return data;
-}
-
-export async function getFormPendaftaranBySiswaIdsAndTahunAjaranId(siswaIds: string[], tahunAjaranId: number): Promise<FormPendaftaran[]> {
-
-  const supabase = await createSupabaseServer();
-  const { data, error } = await supabase
-    .from('form_pendaftaran')
-    .select('*')
-    .is('deleted_at', null)
-    .in('biodata_siswa_id', siswaIds)
-    .eq('tahun_ajaran_id', tahunAjaranId);
 
   if (error) return [];
 
