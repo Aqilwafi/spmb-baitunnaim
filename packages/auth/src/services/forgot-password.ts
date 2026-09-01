ac04ffc65a348456c32a3023144bb80177acbd6b
Aqil Wafi <m.aqil.wafi@gmail.com>
2026-09-01 20:22:37 +0700

    update: forgot pass services

--- packages/auth/src/services/forgot-password.ts
+++ packages/auth/src/services/forgot-password.ts
@@ -1,9 +1,8 @@
 // packages/auth/src/services/forgot-password.ts
-
 import "server-only";
-import { createSupabaseStatic } from "@bn/supabase";
+import { createSupabaseServer } from "@bn/supabase"; // bukan createSupabaseStatic
 
 export async function resetPasswordForEmail(email: string, redirectUrl: string) {
-  const supabase = await createSupabaseStatic();
+  const supabase = await createSupabaseServer();
   return supabase.auth.resetPasswordForEmail(email, { redirectTo: redirectUrl });
 }
